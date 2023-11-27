3198355886ac4962a1312ba51cc59b72287b6935
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-27 14:20:25 +0100

    created functie_plotter.py

---
+++ functie_plotter.py
@@ -0,0 +1,10 @@
+import sys
+
+from PySide6 import QtWidgets
+from PySide6.QtCore import Slot
+import pyqtgraph as pg
+
+
+# PyQtGraph global options
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
